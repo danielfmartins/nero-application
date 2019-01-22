442a281e00bbdbf4fce067d28ea49b66223dc101
dferreiramartins <danielmartins@ivendas.com.br>
2019-01-22 11:02:38 -0200

    refatoracao geral

--- nero/nero-services/src/main/java/com/neroapp/services/config/AppResourceConfiguration.java
+++ nero/nero-services/src/main/java/com/neroapp/services/config/AppResourceConfiguration.java
@@ -1,5 +1,9 @@
 package com.neroapp.services.config;
 
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
+
 import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.jackson.JacksonFeature;
@@ -17,8 +21,17 @@ public class AppResourceConfiguration extends ResourceConfig {
 	public void init() {
 		this.packages("com.neroapp.services")
 				.property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true)
+				.register(ApiListingResource.class)
+				.register(SwaggerSerializers.class)
 				.register(ValidationConfigurationContextResolver.class)
 				.register(JacksonFeature.class);
+		
+		BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1.0.2");
+        beanConfig.setSchemes(new String[]{"http"});
+        beanConfig.setHost("localhost:8080");
+        beanConfig.setBasePath("/*");
+        beanConfig.setResourcePackage("com.neroapp.services");
+        beanConfig.setScan(true);
 	}
-
 }
